a4f5f54f9595f850ad84a2eb6576d813d138a835
dennis-off <dennis_off@web.de>
2024-11-25 08:29:09 +0100

    more playing with production setup

---
+++ svhm-app/env.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+if [ -z "$APP_ENV_PREFIX" ]; then
+    echo "APP_ENV_PREFIX is not set. Exiting."
+    exit 1
+fi
+
+for i in $(env | grep "^$APP_ENV_PREFIX"); do
+    key=$(echo "$i" | cut -d '=' -f 1)
+    value=$(echo "$i" | cut -d '=' -f 2-)
+
+    echo "$key=$value"
+
+    find "/var/www/html/" -type f -exec sed -i 's|'"${key}"'|'"${value}"'|g' {} \;  
+done
--- svhm-deploy/README.md
+++ svhm-deploy/README.md
@@ -1,5 +1,21 @@
 # Schulverein Haseldorfer Marsch – Raus in die Welt
 
+## Konfigurieren
+
 erzeuge .env Datei
 
 und setze die folgenden Variablen
+
+## Starten
+
+docker compose up
+
+## Restore from backup
+
+um backup zu kopieren
+
+docker cp backup.tar.gz svhm-strapi:/opt/app
+
+docker exec -it svhm-strapi bash
+
+yarn strapi import --force -f backup.tar.gz
